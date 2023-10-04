133b7f4c70c7fae9916d2142ad79c632b2f0b994
Chris Lee <chrislee2367@yahoo.com>
2023-10-03 19:16:42 -0500

    changes

--- Data/crashes.py
+++ Data/crashes.py
@@ -4,14 +4,11 @@ import numpy as np
 import sys, argparse, csv
 import matplotlib as mt
 
-def read_cell(x, y):
-    with open('airplane_crashes.csv', 'r') as f:
-        reader = csv.reader(f)
-        y_count = 0
-        for n in reader:
-            if y_count == y:
-                cell = n[x]
-                return cell
-            y_count += 1
-
-print (read_cell(4, 8))
+def read_cell():
+    with open('airplane_crashes.csv') as f:
+        csv_reader = csv.reader(f)
+    for line in csv_reader:
+        l = line['Index']
+        if l == index:
+           crashes= print(line('location'))
+    return(crashes)
